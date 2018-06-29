19b6e99635fc3dc236cda41b27a43bfd40099927
Hector Romojaro <hector.romojaro@gmail.com>
2018-06-29 17:27:19 +0000

    Fix proc and contract doc elements, so they are properly parsed by apidoc::api_*_documentation.
        - @cvs_id -> @cvs-id
        - @version -> @cvs-id
        - @creation_date -> @creation-date
        - @date -> @creation-date
        - Different variations of $Id$
        - Remove wrong @param entry
        - Fix typos

--- tcl/dotlrn-chat-procs.tcl
+++ tcl/dotlrn-chat-procs.tcl
@@ -20,7 +20,7 @@ ad_library {
 
     @author agustin (Agustin.Lopez@uv.es)
     @creation-date 2004-10-10
-    @version $Id: dotlrn-chat-procs.tcl,v 0.1 2004/10/10
+    @cvs-id $Id: dotlrn-chat-procs.tcl,v 0.1 2004/10/10
 
 }
 
